ef30b59a1704a21d5a32ea11f2e12b506dd537e6
Thomas Thurzo <thomas.thurzo@gmail.com>
2022-03-20 14:06:57 +0100

    Fertigstellung Tabelle

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 
+// Start Funktion
 function programmStart(){
     
     let summe = document.getElementById("summeEingabe").value;
@@ -6,22 +7,60 @@ function programmStart(){
     let zinsen = document.getElementById("zinsenEingabe").value;
 
     // Ausgabe Kapitalsumme
-    document.getElementById("kapitalSummeAusgabe").innerHTML = summe + ",-- Euro";
+    document.getElementById("kapitalSummeAusgabe").innerHTML = summe;
 
-    // Berechnung und Ausgabe monatliche Kosten
-    monatlicheKosten = summe * (zinsen / 1200) / (1 - Math.pow((1 + zinsen / 1200), -monate));
-    document.getElementById("monatlicheKosten").innerHTML = Math.round(monatlicheKosten) + ",-- Euro";
+    // Ausgabe monatliche Kosten
+    document.getElementById("monatlicheKosten").innerHTML = monatlicheKostenBerechnung(summe, monate, zinsen);
 
-    // Berechnung und Ausgabe Total Zinsen
-    totalZinsen = (summe * zinsen/1200);
-    document.getElementById("zinsenAusgabe").innerHTML = totalZinsen + ",-- Euro";
-    
+    // Ausgabe Zinsen Total
+
+    // Ausgabe Kosten Total
+ 
+
+    // Berechnung und Ausgabe Table
+    displayTable(summe, monate, zinsen, monatlicheKosten);  
 }
 
 
+// Berechnung für die monatlichen Kosten
+function monatlicheKostenBerechnung(summe, monate, zinsen){
+    let monatlicheKosten = summe * (zinsen / 1200) / (1 - Math.pow((1 + zinsen / 1200), -monate));
+
+    return monatlicheKosten;
+}
 
 
-function display(){
+// Ausgabe Table
+function displayTable(summe, monate, zinsen){
     
+    let monatlicheKosten = monatlicheKostenBerechnung(summe, monate, zinsen).toFixed(2);
+    let kapitalRückzahlung = monatlicheKosten;
+    let zinsenRückzahlung = summe * zinsen / 1200;
+    let totalZinsen = 0;
+    
+    let tableRow = "";
+    let monateSpalte = "";
+    let montalicheZahlungenSpalte = "<td>" + monatlicheKosten + "</td>";
+    let kapitalRückzahlungSpalte = "";
+    let zinsenRückzahlungSpalte = "";
+    let totalZinsenSpalte = "";
+    let summeSpalte = "";
+    
+    for (let index = 1; index <= monate; index++) {
+
+        zinsenRückzahlung = (summe * zinsen / 1200).toFixed(2);
+        kapitalRückzahlung = (monatlicheKosten - zinsenRückzahlung).toFixed(2);
+        summe = (summe - kapitalRückzahlung).toFixed(2);
+        totalZinsen = totalZinsen + parseFloat(zinsenRückzahlung);
+        monateSpalte = "<td>" + index + "</td>";
+        kapitalRückzahlungSpalte = "<td>" + kapitalRückzahlung + "</td>";
+        zinsenRückzahlungSpalte = "<td>" + zinsenRückzahlung + "</td>";
+        totalZinsenSpalte = "<td>" + totalZinsen + "</td>";
+        summeSpalte = "<td>" + summe + "</td>";
+        
+        tableRow += "<tr>" + monateSpalte + montalicheZahlungenSpalte + kapitalRückzahlungSpalte + zinsenRückzahlungSpalte + totalZinsenSpalte + summeSpalte + "</tr>";
+        document.getElementById("tableAusgabe").innerHTML = tableRow;
+        
+    }
 }
 
