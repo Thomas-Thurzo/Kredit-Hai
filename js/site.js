68a948501d852d4bdb6c21680ecf81e26c8bbba0
Thomas Thurzo <thomas.thurzo@gmail.com>
2022-03-20 18:45:34 +0100

    Fertigstellung

--- js/site.js
+++ js/site.js
@@ -1,4 +1,8 @@
 
+// Formatierung für Euro Währung
+let formatter = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR"});
+
+
 // Start Funktion
 function programmStart(){
     
@@ -6,17 +10,23 @@ function programmStart(){
     let monate = document.getElementById("monateEingabe").value;
     let zinsen = document.getElementById("zinsenEingabe").value;
 
+    // Umwandeln der Eingabewerte in Zahlentypen
+    summe = parseFloat(summe);
+    monate = parseInt(monate);
+    zinsen = parseFloat(zinsen);
+
     // Ausgabe Kapitalsumme
-    document.getElementById("kapitalSummeAusgabe").innerHTML = summe;
+    document.getElementById("kapitalSummeAusgabe").innerHTML = formatter.format(summe);
 
     // Ausgabe monatliche Kosten
-    document.getElementById("monatlicheKosten").innerHTML = monatlicheKostenBerechnung(summe, monate, zinsen);
+    document.getElementById("monatlicheKosten").innerHTML = formatter.format(monatlicheKostenBerechnung(summe, monate, zinsen));
 
     // Ausgabe Zinsen Total
+    document.getElementById("zinsenAusgabe").innerHTML = formatter.format(gesamtZinsenBerechnung(summe, monate, zinsen));
 
     // Ausgabe Kosten Total
+    document.getElementById("kostenAusgabe").innerHTML = formatter.format(summe + gesamtZinsenBerechnung(summe, monate, zinsen));
  
-
     // Berechnung und Ausgabe Table
     displayTable(summe, monate, zinsen, monatlicheKosten);  
 }
@@ -29,18 +39,37 @@ function monatlicheKostenBerechnung(summe, monate, zinsen){
     return monatlicheKosten;
 }
 
+// Berechnung für die gesammten Zinsen
+function gesamtZinsenBerechnung(summe, monate, zinsen){
+
+       // Variablen
+       let monatlicheKosten = monatlicheKostenBerechnung(summe, monate, zinsen);
+       let kapitalRückzahlung = monatlicheKosten;
+       let zinsenRückzahlung = summe * zinsen / 1200;
+       let totalZinsen = 0; 
+
+    for (let index = 1; index <= monate; index++) {
+        zinsenRückzahlung = (summe * zinsen / 1200);
+        kapitalRückzahlung = (monatlicheKosten - zinsenRückzahlung);
+        summe = (summe - kapitalRückzahlung);
+        totalZinsen = totalZinsen + zinsenRückzahlung;       
+    }
+
+    return totalZinsen;
+}
+
 
 // Ausgabe Table
 function displayTable(summe, monate, zinsen){
     
-    let monatlicheKosten = monatlicheKostenBerechnung(summe, monate, zinsen).toFixed(2);
+    // Variablen
+    let monatlicheKosten = monatlicheKostenBerechnung(summe, monate, zinsen);
     let kapitalRückzahlung = monatlicheKosten;
     let zinsenRückzahlung = summe * zinsen / 1200;
-    let totalZinsen = 0;
-    
+    let totalZinsen = 0; 
     let tableRow = "";
     let monateSpalte = "";
-    let montalicheZahlungenSpalte = "<td>" + monatlicheKosten + "</td>";
+    let montalicheZahlungenSpalte = "<td>" + formatter.format(monatlicheKosten) + "</td>";
     let kapitalRückzahlungSpalte = "";
     let zinsenRückzahlungSpalte = "";
     let totalZinsenSpalte = "";
@@ -48,17 +77,23 @@ function displayTable(summe, monate, zinsen){
     
     for (let index = 1; index <= monate; index++) {
 
-        zinsenRückzahlung = (summe * zinsen / 1200).toFixed(2);
-        kapitalRückzahlung = (monatlicheKosten - zinsenRückzahlung).toFixed(2);
-        summe = (summe - kapitalRückzahlung).toFixed(2);
-        totalZinsen = totalZinsen + parseFloat(zinsenRückzahlung);
+        // Berechnungen
+        zinsenRückzahlung = (summe * zinsen / 1200);
+        kapitalRückzahlung = (monatlicheKosten - zinsenRückzahlung);
+        summe = (summe - kapitalRückzahlung);
+        totalZinsen = totalZinsen + zinsenRückzahlung;
+
+        // Erzeugen der einzelnen Spalten
         monateSpalte = "<td>" + index + "</td>";
-        kapitalRückzahlungSpalte = "<td>" + kapitalRückzahlung + "</td>";
-        zinsenRückzahlungSpalte = "<td>" + zinsenRückzahlung + "</td>";
-        totalZinsenSpalte = "<td>" + totalZinsen + "</td>";
-        summeSpalte = "<td>" + summe + "</td>";
+        kapitalRückzahlungSpalte = "<td>" + formatter.format(kapitalRückzahlung) + "</td>";
+        zinsenRückzahlungSpalte = "<td>" + formatter.format(zinsenRückzahlung) + "</td>";
+        totalZinsenSpalte = "<td>" + formatter.format(totalZinsen) + "</td>";
+        summeSpalte = "<td>" + formatter.format(summe) + "</td>";
         
+        // Erzeugen der Reihe
         tableRow += "<tr>" + monateSpalte + montalicheZahlungenSpalte + kapitalRückzahlungSpalte + zinsenRückzahlungSpalte + totalZinsenSpalte + summeSpalte + "</tr>";
+        
+        // Ausgabe der Reihe
         document.getElementById("tableAusgabe").innerHTML = tableRow;
         
     }
